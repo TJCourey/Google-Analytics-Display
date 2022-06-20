5d4139ec197bca54bfced2f4fd5e1ce4d67544c1
TJ Courey <tjcourey84@gmail.com>
2022-06-20 15:06:02 -0600

    initialized node and installed dependencies

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
 // add universal variables for api calls
+
 //  add api key
+
+
 //  add clients GA4 to collect info from
-// call google api
+    // call google api
+  /**
+   * TODO(developer): Uncomment this variable and replace with your
+   *   Google Analytics 4 property ID before running the sample.
+   */
+  // propertyId = 'YOUR-GA4-PROPERTY-ID';
+
+  // Imports the Google Analytics Data API client library.
+  const {BetaAnalyticsDataClient} = require('@google-analytics/data');
+
+  // Using a default constructor instructs the client to use the credentials
+  // specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
+  const analyticsDataClient = new BetaAnalyticsDataClient();
+
+  // Runs a simple report.
+  async function runReport() {
+    const [response] = await analyticsDataClient.runReport({
+      property: `properties/${propertyId}`,
+      dateRanges: [
+        {
+          startDate: '2020-03-31',
+          endDate: 'today',
+        },
+      ],
+      dimensions: [
+        {
+          name: 'city',
+        },
+      ],
+      metrics: [
+        {
+          name: 'activeUsers',
+        },
+      ],
+    });
+
+    console.log('Report result:');
+    response.rows.forEach(row => {
+      console.log(row.dimensionValues[0], row.metricValues[0]);
+    });
+  }
+
+  runReport();
 // set data on html page
